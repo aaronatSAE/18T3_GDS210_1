187637e7acc3b6245709022f0e40901df30115d6
Antony-Clements <40747712+Antony-Clements@users.noreply.github.com>
2018-10-23 16:33:35 +1100

    thrown object collision done
    thrown objects enable, move forward, and disable when they hit objects tagged ground or enemy

---
+++ 18T3 GDS210/Assets/Scripts/BulletCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision CollisionInfo)
+    {
+        if(CollisionInfo.transform.tag == "Ground" || CollisionInfo.transform.tag == "Enemy")
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}

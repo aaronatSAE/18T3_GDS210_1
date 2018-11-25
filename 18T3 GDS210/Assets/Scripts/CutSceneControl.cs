070eb6a4dddfa6cd8cdc0b61fe7044586c9e08f6
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2018-11-25 16:52:30 +1100

    changed cutscenecontrol
    cutscene control now uses ienumerators to control scene transition rather than multiple functions.

--- 18T3 GDS210/Assets/Scripts/CutSceneControl.cs
+++ 18T3 GDS210/Assets/Scripts/CutSceneControl.cs
@@ -4,35 +4,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using SpeechLib;
+using UnityEngine.SceneManagement;
 
 public class CutSceneControl : MonoBehaviour
 {
-    public Image[] CutSceneOrder;
+    public Canvas canvas;
+    public GameObject[] CutSceneOrder;
+    public Sprite[] CutSceneSprite;
+    public Image[] CutSceneImage;
     private string Text;
+    private int i;
 
     SpVoice Voice = new SpVoice();
 
-    void Start ()
+    void Start()
     {
-        CutSceneOrder = FindObjectsOfType<Image>();
+        canvas = FindObjectOfType<Canvas>();
+
+        for (i = 0; i < CutSceneOrder.Length; i++)
+        {
+            CutSceneOrder[i] = new GameObject("Scene " + i);
+            CutSceneOrder[i].transform.SetParent(canvas.transform);
+            CutSceneOrder[i].AddComponent<Image>();
+            CutSceneOrder[i].GetComponent<Image>().rectTransform.sizeDelta = new Vector2(1148, 603);
+            CutSceneOrder[i].GetComponent<Image>().rectTransform.localPosition = new Vector3(0, 0, 0);
+            CutSceneOrder[i].GetComponent<Image>().sprite = CutSceneSprite[i];
+            CutSceneImage[i] = CutSceneOrder[i].GetComponent<Image>();
+
+        }
+
+        i = 0;
         Text = "hello world";
-        Invoke("FirstFade", 2.0f);
+        StartCoroutine(Fade());
     }
 
-    void FirstFade()
+    IEnumerator Fade()
     {
-        Invoke("Speak", 2.0f);
-        CutSceneOrder[0].CrossFadeAlpha(0, 2.0f, false);
-        CutSceneOrder[1].CrossFadeAlpha(1, 2.0f, false);
+        StopCoroutine(CrossFade());
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(CrossFade());
 
-        Invoke("SecondFade", 4.0f);
     }
 
-    void SecondFade()
+    IEnumerator CrossFade()
     {
-        Invoke("Speak", 2.0f);
-        CutSceneOrder[1].CrossFadeAlpha(0, 2.0f, false);
-        CutSceneOrder[2].CrossFadeAlpha(1, 2.0f, false);
+        StopCoroutine(Fade());
+        yield return new WaitForSeconds(2);
+        Debug.Log(i);
+
+        CutSceneImage[i].CrossFadeAlpha(0, 2.0f, false);
+
+        if (i < CutSceneOrder.Length - 1)
+        {
+            i++;
+            CutSceneImage[i].CrossFadeAlpha(1, 2.0f, false);
+            StartCoroutine(Fade());
+        }
+        else
+        {
+            yield return new WaitForSeconds(2);
+            if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCount)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+            }
+            else
+            {
+                SceneManager.LoadScene(1, LoadSceneMode.Single);
+            }
+        }
     }
 
     public void Speak()

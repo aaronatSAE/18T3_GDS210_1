b9718d2619900feef30db71f25e2469a8332266c
a-clements <40747712+Antony-Clements@users.noreply.github.com>
2018-12-13 19:57:18 +1100

    replaced platforms on level 1

--- 18T3 GDS210/Assets/Scripts/ChildOfPlatform.cs
+++ 18T3 GDS210/Assets/Scripts/ChildOfPlatform.cs
@@ -7,7 +7,7 @@ public class ChildOfPlatform : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        this.transform.position = new Vector3(this.transform.parent.position.x, this.transform.parent.position.y + 0.2f, this.transform.parent.position.z);
+        this.transform.position = new Vector3(this.transform.parent.position.x, this.transform.parent.position.y + 0.9f, this.transform.parent.position.z);
         this.transform.localScale = this.transform.parent.localScale;
 	}
 	
